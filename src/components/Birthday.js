1782238b29ef087e7e936970b102e723149659ab
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-09-20 15:57:31 +1000

    Update the data.json file and write a function to display birthday cakes as cards on the Birthday page.

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Assuming you're using routing for links
+import data from '../data/data.json'; // Assuming the data.js file is in the same folder
+import './Card.css'; // Assuming you have styles for the card
 
-const Birthday = (props) => {
-    return <h1>Birthday Page!!!!</h1>
-}
- 
+const Birthday = () => {
+    // Filter the cakes with type "Birthday-cake"
+    const birthdayCakes = data.filter(cake => cake.type === "Birthday-cake");
+
+    return (
+        <div className="card-container">
+            {birthdayCakes.map(cake => (
+                <div key={cake.id} className={cake.animation}>
+                    <Link to={`/product/${cake.id}`}>
+                        <div className="front">
+                            <img src={`/images/${cake.id}.jpg`} alt={cake.title} className="card-image" />
+                            <div className="container">
+                                <h3>{cake.title}</h3>
+                                <h3><span className="price">${cake.price}</span></h3>
+                                <p>{cake.description}</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    );
+};
+
 export default Birthday;
